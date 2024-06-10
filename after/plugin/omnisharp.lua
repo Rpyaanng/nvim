48102fd43391cb06602b4cdfec3badb8cf80e5a0
Rpyaanng <RyanPang12@gmail.com>
2024-06-10 10:18:00 -0600

    Made a config for neovim for vscode

--- after/plugin/omnisharp.lua
+++
@@ -1,5 +0,0 @@
-local lspconfig = require("lspconfig")
-
-lspconfig.omnisharp.setup {
-  cmd = { 'omnisharp', '--languageserver', '--hostPID', tostring(vim.fn.getpid()) }, -- Assuming you have an on_attach function. Delete this line if you don't.
-}
